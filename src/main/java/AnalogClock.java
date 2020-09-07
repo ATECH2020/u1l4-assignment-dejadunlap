3c3aca3aa175cd5c1feb6b4457adcab7836cb98b
dejadunlap <70656325+dejadunlap@users.noreply.github.com>
2020-09-06 18:51:17 -0700

    Update AnalogClock.java

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -1,19 +1,28 @@
 import java.util.Scanner;
-
+ 
 class AnalogClock {
-    public static void main(String[] args) {
-
-        //Creates a new scanner for user input
-        Scanner scanner = new Scanner(System.in);
-
-        //Reads ints from user
-
-
-        /*
-         *  your code goes here
-         */
-
-        // closing the scanner object
-        scanner.close();
-    }
+   public static void main(String[] args) {
+ 
+       //Creates a new scanner for user input
+       Scanner scanner = new Scanner(System.in);
+ 
+       //Reads ints from user
+       int degree = scanner.nextInt();
+ 
+       /*
+        *  your code goes here
+        */
+        int hour = degree / 30;
+        int remainder = degree % 30; 
+ 
+        double minutes = (remainder/30.0) * 60;
+ 
+        double minutesDegree = minutes * 6;
+ 
+        System.out.print(minutesDegree);
+ 
+       // closing the scanner object
+       scanner.close();
+   }
 }
+
