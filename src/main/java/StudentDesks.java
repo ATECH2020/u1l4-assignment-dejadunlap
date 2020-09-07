3602d9fa94ed5bc22b8dad4f3ff4a9053859c97d
dejadunlap <70656325+dejadunlap@users.noreply.github.com>
2020-09-06 18:58:55 -0700

    Update StudentDesks.java

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -1,20 +1,48 @@
 import java.util.Scanner;
-
+ 
 class StudentDesks {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
+ 
+       //Creates a new scanner for user input
+       Scanner scanner = new Scanner(System.in);
+ 
+       //Reads ints from user
+       int classA = scanner.nextInt();
+       int classB = scanner.nextInt();
+       int classC = scanner.nextInt();
+       int deskA, deskB, deskC;
+ 
+       /*
+        *  your code goes here
+        */
+       int isEvenA = classA % 2;
+       int isEvenB = classB % 2;
+       int isEvenC = classC % 2;
 
-        //Creates a new scanner for user input
-        Scanner scanner = new Scanner(System.in);
+ 
+       if (isEvenA == 1){
+         deskA = (classA / 2) + 1;
+       } else {
+         deskA = classA / 2;
+       }
 
-        //Reads ints from user
-        
+       if (isEvenB == 1){
+         deskB = (classB / 2) + 1;
+       } else {
+         deskB = classB / 2;
+       }
 
-        /*
-         *  your code goes here
-         */
+       if (isEvenC == 1){
+         deskC = (classC / 2) + 1;
+       } else {
+         deskC = classC / 2;
+       }
 
-
-        // closing the scanner object
-        scanner.close();
-    }
+       int totalDesk = deskA + deskB + deskC;
+ 
+       System.out.print(totalDesk);
+ 
+       // closing the scanner object
+       scanner.close();
+   }
 }
