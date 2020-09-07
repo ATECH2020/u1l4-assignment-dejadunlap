da66a39992b0dd20e8ac72efe1de6d6814b8d576
dejadunlap <70656325+dejadunlap@users.noreply.github.com>
2020-09-06 18:59:27 -0700

    Update Timestamps.java

--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -1,26 +1,34 @@
 import java.util.Scanner;
-
+ 
 class Timestamps {
-    public static void main(String[] args) {
-
-        //Creates a new scanner for user input
-        Scanner scanner = new Scanner(System.in);
-
-        //Reads ints from user
-        int hours1 = scanner.nextInt();
-        int minutes1 = scanner.nextInt();
-        int seconds1 = scanner.nextInt();
-        int hours2 = scanner.nextInt();
-        int minutes2 = scanner.nextInt();
-        int seconds2 = scanner.nextInt();
-
-
-        /*
-         *  your code goes here
-         */
-
-
-        // closing the scanner object
-        scanner.close();
-    }
+   public static void main(String[] args) {
+ 
+       //Creates a new scanner for user input
+       Scanner scanner = new Scanner(System.in);
+ 
+       //Reads ints from user
+       int hours1 = scanner.nextInt();
+       int minutes1 = scanner.nextInt();
+       int seconds1 = scanner.nextInt();
+       int hours2 = scanner.nextInt();
+       int minutes2 = scanner.nextInt();
+       int seconds2 = scanner.nextInt();
+ 
+       /*
+        *  your code goes here
+        */
+      
+       int differenceH = hours2 - hours1;
+       int differenceM = minutes2 - minutes1;
+       int differenceS = seconds2 - seconds1;
+ 
+       int convertH = differenceH * 3600;
+       int convertM = differenceM * 60;
+ 
+       int totalDifference = convertH + convertM + differenceS;
+ 
+       System.out.print(totalDifference);
+       // closing the scanner object
+       scanner.close();
+   }
 }
