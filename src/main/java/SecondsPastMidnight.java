9b2f29b3d293210d8516abb476a07cf8c6774371
dejadunlap <70656325+dejadunlap@users.noreply.github.com>
2020-09-06 18:52:01 -0700

    Update SecondsPastMidnight.java

--- src/main/java/SecondsPastMidnight.java
+++ src/main/java/SecondsPastMidnight.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
-
+ 
 class SecondsPastMidnight {
-    public static void main(String[] args) {
-
-        //Creates a new scanner for user input
-        Scanner scanner = new Scanner(System.in);
-
-        //Reads int from user
-        int number = scanner.nextInt();
-
-        //Your code goes here
-
-
-        // closing the scanner object
-        scanner.close();
-    }
+   public static void main(String[] args) {
+ 
+       //Creates a new scanner for user input
+       Scanner scanner = new Scanner(System.in);
+ 
+       //Reads int from user
+       int seconds = scanner.nextInt();
+ 
+       //Your code goes here
+       int hours = seconds / 3600; //hours since midnight
+       int minutes = seconds / 60; //minutes since midnight
+ 
+       System.out.print(hours + " " + minutes);
+       // closing the scanner object
+       scanner.close();
+   }
 }
